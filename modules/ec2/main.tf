e3eb37a14b6590f7473b55e71e8a3fed91266cfd
Sireeshalatha Bangaru <sireesha1885@gmail.com>
2023-09-11 08:44:06 +0000

    Updated main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -8,6 +8,13 @@ resource "aws_instance" "demo_instance" {
     "Name" = var.name
     "Env"  = var.env
   }
+  provider "aws" {
+  region = "us-west-2"  # Replace this with your desired AWS region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+
+  # Other optional configuration settings can be added here if needed
+}
 
 
 }
